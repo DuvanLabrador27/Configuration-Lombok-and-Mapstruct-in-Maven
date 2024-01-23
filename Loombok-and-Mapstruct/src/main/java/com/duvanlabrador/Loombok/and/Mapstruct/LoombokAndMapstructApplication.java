1c327eb29e3000a1fa881ce2263cced41bc75093
Duvan Labrador <carlosduvanlh@ufps.edu.co>
2024-01-23 14:24:08 -0500

    Add Project

---
+++ Loombok-and-Mapstruct/src/main/java/com/duvanlabrador/Loombok/and/Mapstruct/LoombokAndMapstructApplication.java
@@ -0,0 +1,13 @@
+package com.duvanlabrador.Loombok.and.Mapstruct;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LoombokAndMapstructApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LoombokAndMapstructApplication.class, args);
+	}
+
+}
---
+++ Loombok-and-Mapstruct/src/test/java/com/duvanlabrador/Loombok/and/Mapstruct/LoombokAndMapstructApplicationTests.java
@@ -0,0 +1,13 @@
+package com.duvanlabrador.Loombok.and.Mapstruct;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LoombokAndMapstructApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
